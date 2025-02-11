34e2107c59e714866747b854d9add0a43890a57d
Jane Shimmer <jane@j4.pm>
2025-02-11 11:59:05 -0500

    i love fixing patches

--- src/roleColoredMessages/index.tsx
+++ src/roleColoredMessages/index.tsx
@@ -4,7 +4,7 @@ const pastelize = moonlight.getConfigOption<boolean>("roleColoredMessages", "pas
 
 export const patches: Patch[] = [
   {
-    find: ".SOURCE_MESSAGE_DELETED",
+    find: ".SOURCE_MESSAGE_DELETED)",
     replace: {
       match: /"div",{id:/,
       replacement: `"div",{style:{color:require("nameColor_colorUtils").default.getRoleColor({messageId:arguments[0].message.id,channelId:arguments[0].message.channel_id,userId:arguments[0].message.author.id,saturation:${pastelize ? 85 : null},value:${pastelize ? 75 : null}})},id:`
